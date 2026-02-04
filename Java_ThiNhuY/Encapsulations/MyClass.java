9ef7c142c11ca3205c0d2251e3d450d068d2c156
Naarah-dev <thinhuy1@dtu.edu.vn>
2026-02-04 10:38:38 +0700

    Add files via upload
    First code demo

---
+++ Java_ThiNhuY/Encapsulations/MyClass.java
@@ -0,0 +1,9 @@
+package Encapsulations;
+
+public class MyClass {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setName("John");
+        System.out.println(p.getName());
+    }
+}
---
+++ Java_ThiNhuY/TongHaiSo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TongHaiSo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhap so a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Nhap so b: ");
+        int b = scanner.nextInt();
+
+        int tong = a + b;
+
+        System.out.println("Tong hai so a va b = " + tong);
+
+        scanner.close();
+    }
+}
